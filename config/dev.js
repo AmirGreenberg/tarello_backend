70d0db420bfdbcf8e350df8b6ebcee0b46e4a263
Amir Greenberg <amirgggg@gmail.com>
2024-01-28 12:19:38 +0200

    change db

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 // import { dbUserName, dbPassword } from '../private/privateKeys.service.js';
 
 export default {
-    dbURL: process.env.MONGODB_URI || "mongodb+srv://tarello_amir:tarello_amir123@tarello-amir.z5tdosx.mongodb.net/",
+    dbURL: 'mongodb+srv://tarello_amir:tarello_amir123@tarello-amir.z5tdosx.mongodb.net/',
     dbName: 'tarello_db',
 }
 
